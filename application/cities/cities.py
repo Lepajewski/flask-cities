ea32ee02941b50731aeed11ce2a607f82b59f4bf
lukaszk <lukaszkania.pba@gmail.com>
2021-12-13 22:50:40 +0100

    get method

--- application/cities/cities.py
+++ application/cities/cities.py
@@ -1,6 +1,6 @@
 #coding: utf-8
 
-from flask import render_template, url_for, redirect, request
+from flask import render_template, url_for, redirect, request, json
 from ..models import Cities
 from . import bp_cities
 
@@ -11,8 +11,11 @@ def index():
 @bp_cities.route('/cities', methods=['GET', 'POST'])
 def view_cities():
     if request.method == 'GET':
-        cities_list = {i:Cities.get(Cities.id) for i in Cities.get(Cities.name)}
-        return render_template('cities.html')
+        cities_json = {}
+        for x in Cities:
+            cities_json[x.id] = x.name
+
+        return render_template('cities.html', cities_list=cities_json)
     else:
         try:
             city = Cities.create(name = request.form['name'])
